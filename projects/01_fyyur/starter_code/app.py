32a41d536b35401ec4d41483c2c1806a853fb538
Jason Kang <ishk001@bucknell.edu>
2021-09-08 22:28:22 -0700

    started show form

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -47,8 +47,8 @@ class Venue(db.Model):
     # adding missing fields 9/8/21
     genres = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
-    looking_for_talent = db.Column(db.Boolean)
-    seeking_desc = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -65,11 +65,14 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
     # adding missing fields 9/8/21
     website_link = db.Column(db.String(120))
-    looking_for_venues = db.Column(db.Boolean)
-    seeking_desc = db.Column(db.String)
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
 
+  artist_id = db.Column(db.Integer)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
